17356c24ef48b77ef68e95d7b72ccfe3bcd031c3
Aaron Anderson <Aanderson@katmaicorp.com>
2017-10-25 15:22:57 -0400

    First commit

---
+++ Assets/Scripts/LoadLesson.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLesson : MonoBehaviour {
+
+	// Load correct scene
+    public void Load(string _name) {
+        Application.LoadLevel(_name);
+    }
+
+}
